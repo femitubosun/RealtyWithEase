ae43fd38a605ebe6111b2eb35a36e05776feb382
Glory-Lyka Okposin <lykaedem11@gmail.com>
2023-07-10 10:54:27 +0100

    feat: add landlord signup
    BREAKING CHANGE:

---
+++ user_management/forms/landlord_signup_form.py
@@ -0,0 +1,14 @@
+from django.forms import Form, CharField, ChoiceField, EmailField
+
+from user_management.models.user_profile import GENDER_CHOICES
+
+class LandlordSignupForm(Form):
+    '''
+    Class describing the Landlord Signup Form
+    '''
+    email = EmailField(label="Email", max_length=100)
+    first_name = CharField(label="First Name", max_length=100)
+    last_name = CharField(label="Last Name", max_length=100)
+    password = CharField(label="Password", max_length=100)
+    gender = ChoiceField(label="Gender", choices=GENDER_CHOICES)
+    
--- user_management/urls.py
+++ user_management/urls.py
@@ -5,5 +5,5 @@ from user_management.views import signup_views
 urlpatterns = [
     path("signup/tenant", signup_views.sign_up_as_tenant, name="sign-up-tenant"),
     path("signup/agent", signup_views.sign_up_as_agent, name="sign-up-agent"),
-
-]
+    path("signup/landlord", signup_views.sign_up_as_landlord,name="sign-up-landlord"),
+    ]
