989dbc1548ad8a30fd4687f10c0fe7bb20d080a1
Femi Olatubosun | ƒa†3 <femi@zerocomplex.ai>
2023-08-31 20:41:26 +0100

    feat: add send welcome email to signup flow

--- core/infrastructure/internal/mail_client.py
+++ core/infrastructure/internal/mail_client.py
@@ -1,13 +1,21 @@
 from typing import List
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
 class MailClient:
     @staticmethod
-    def send_email(subject: str, message: str, from_email: str, recipients: List[str]):
-        return send_mail(subject, message, from_email, recipient_list=recipients)
+    def send_email(subject: str, message: str, from_email: str, recipients: List[str],
+                   html_message: str = None):  # type: ignore # noqa: ignore
+        return send_mail(subject, message, from_email, recipient_list=recipients, html_message=html_message)
 
     @staticmethod
-    def send_welcome_email(email: str, token: str):
-        print(token)
-        return MailClient.send_email("Welcome To Realty With Ease", 'Welcome', 'tech@realtywithease.com', [email])
+    def send_welcome_email(email: str, first_name: str, token: str):
+        welcome_email_template = 'user_management/emails/welcome_mail.html'
+
+        html_message = render_to_string(welcome_email_template, {"first_name": first_name, "token": token})
+
+        return MailClient.send_email("Welcome To Realty With Ease", strip_tags(html_message), 'tech@realtywithease.com',
+                                     [email],
+                                     html_message)
