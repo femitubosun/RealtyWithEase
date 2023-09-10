746ac8c83c01b0123ac42d341070b60d438f9f68
Glory-Lyka Okposin <lykaedem11@gmail.com>
2023-09-10 14:25:30 +0100

    feat: added handler for reset password and configure url

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from user_management.views import signup_views, authentication_views, user_profile_views
+from user_management.views import signup_views, authentication_views, user_profile_views, reset_password_views
 
 urlpatterns = [
     path("signup/tenant", signup_views.sign_up_as_tenant, name="sign-up-tenant"),
     path("signup/agent", signup_views.sign_up_as_agent, name="sign-up-agent"),
     path("signup/landlord", signup_views.sign_up_as_landlord, name="sign-up-landlord"),
     path('authenticate/user', authentication_views.authenticate_user, name='authenticate-user'),
-    path('user-profile', user_profile_views.get_user_profile, name='get-user-profile`')
+    path('user-profile', user_profile_views.get_user_profile, name='get-user-profile`'),
+    path('send_otp_email/', reset_password_views.send_reset_otp_email, name='send_reset_otp_email'),
+    path('reset_password/', reset_password_views.reset_password, name='reset_password')
 ]
+
+
--- user_management/views/__init__.py
+++ user_management/views/__init__.py
@@ -1,2 +1,3 @@
 from .user_profile_views import get_user_profile
-from .authentication_views import authenticate_user
+from .authentication_views import authenticate_user 
+from .reset_password_views import send_reset_otp_email, reset_password
